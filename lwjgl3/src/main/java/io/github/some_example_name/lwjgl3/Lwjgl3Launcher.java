f8b115e2e0761c10090017f065d80ad436695cc4
maksa <xavramenkom1@stuba.sk>
2026-03-15 17:25:41 +0100

    15.03

--- lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/Lwjgl3Launcher.java
@@ -1,8 +1,8 @@
-package io.xavramenkom.lwjgl3;
+package io.github.some_example_name.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.xavramenkom.Main;
+import io.github.some_example_name.Main;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
